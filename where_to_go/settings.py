018613567b30cbc6d094f4fd40e406dfe12521f0
anna <annslob@gmail.com>
2026-05-07 20:15:43 +0300

    add frontend

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'where_to_go',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'where_to_go.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'where_to_go', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
